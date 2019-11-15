1b3eec0145f5fc1f2799f3d95a55245056dcf16f
11701902 <k11701902@students.jku.at>
2019-11-15 11:41:11 +0100

    telegraf test

--- api/src/main/java/at/itproject/api/ApiController.java
+++ api/src/main/java/at/itproject/api/ApiController.java
@@ -5,8 +5,12 @@ import io.swagger.client.api.HistoryApi;
 import io.swagger.client.api.PrinterApi;
 import io.swagger.client.model.EventHistoryEntry;
 import io.swagger.client.model.PrintJobHistory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,8 +20,8 @@ import java.util.List;
 public class ApiController {
 
     @GetMapping
-    public String test() {
-
+    @ResponseBody public ResponseEntity<String> test() {
+/*
         HistoryApi historyApi = new HistoryApi();
         PrinterApi printerApi = new PrinterApi();
         printerApi.getApiClient().setBasePath("");
@@ -32,6 +36,8 @@ public class ApiController {
 
 
         return string;
+        */
+ return new ResponseEntity<>("printjob_test, temperature=82 1465839830100400200", HttpStatus.OK);
     }
 
 }
